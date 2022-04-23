51fa4be09d7212fbb409568242e9fb09e15c29d8
Yohanes Silitonga <yohanesrfsilitonga21@gmail.com>
2022-04-23 16:17:52 +0700

    Update packages & components

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      HeroBanner
+      <div className="products-heading">
+        <h2>Best Selling Product</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
+      Footer
+    </>
+  );
+};
+
+export default Home;
