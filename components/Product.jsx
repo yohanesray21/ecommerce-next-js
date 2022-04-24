27473211a52f067ca27474b0a85920608e3b9845
Yohanes Silitonga <yohanesrfsilitonga21@gmail.com>
2022-04-24 16:29:25 +0700

    Update product

---
+++ components/Product.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+import { urlFor } from "../lib/client";
+
+const Product = ({ product }) => {
+  return <div>{console.log(product)}</div>;
+};
+
+export default Product;
