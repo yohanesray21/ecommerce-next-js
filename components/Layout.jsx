ce24fc1583c07629f0c114343f546ecbe1d6bcdf
Yohanes Silitonga <yohanesrfsilitonga21@gmail.com>
2022-04-28 21:54:18 +0700

    Update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
     <div>
       <div className="layout">
         <Head>
-          <title>JS Mastery Store</title>
+          <title>E-commerce</title>
         </Head>
         <header>
           <Navbar />
